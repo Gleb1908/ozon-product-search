f945ad33fa082c9fc950613d2704381723be730c
Gleb1908 <gleb1908@list.ru>
2021-10-17 22:53:10 +0300

    added HomePage.java

---
+++ src/main/java/pages/HomePage.java
@@ -0,0 +1,36 @@
+package pages;
+
+import products.Products;
+import org.junit.jupiter.api.Assertions;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class HomePage extends BasePage {
+
+
+    @FindBy(xpath = "//div[@class='f9j4']/input[@name]")
+    private WebElement searchLine;
+
+
+    /**
+     * Поиск товара в строке поиска
+     * @param product - имя товара
+     * @return ResultsPage
+     */
+    public SearchingResultsPage productSearch(Products product) {
+        return productSearch(product.getTitle());
+    }
+
+    public SearchingResultsPage productSearch(String product) {
+        waitUtilElementToBeClickable(searchLine);
+        searchLine.click();
+        searchLine.clear();
+        searchLine.sendKeys(product);
+        Assertions.assertEquals(searchLine.getAttribute("value"),product,"Введенный текст не совпадает");
+        searchLine.sendKeys(Keys.ENTER);
+        pageManager.getSearchingResultsPage().checkOpenSearchingResultPage(product);
+        return pageManager.getSearchingResultsPage();
+    }
+
+}
