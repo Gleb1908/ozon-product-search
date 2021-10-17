590e83646d96cd3bfe976d0eb65e1a9f4f7f63cf
Gleb1908 <gleb1908@list.ru>
2021-10-17 22:54:50 +0300

    added PageManager.java

---
+++ src/main/java/managers/PageManager.java
@@ -0,0 +1,81 @@
+package managers;
+
+import pages.SearchingResultsPage;
+import pages.HomePage;
+import pages.ShoppingBasketPage;
+
+public class PageManager {
+
+    /**
+     * Менеджер страничек
+     */
+    private static PageManager pageManager;
+
+    /**
+     * Стартовая страничка
+     */
+    private HomePage homePage;
+
+    /**
+     * Страничка с результатами поиска
+     */
+    private SearchingResultsPage searchingResultsPage;
+
+    /**
+     * Страничка корзины с покупками
+     */
+    private ShoppingBasketPage shoppingBasketPage;
+
+    /**
+     * Конструктор специально был объявлен как private (singleton паттерн)
+     *
+     * @see PageManager#getPageManager()
+     */
+    private PageManager() {
+    }
+
+    /**
+     * Ленивая инициализация PageManager
+     *
+     * @return PageManager
+     */
+    public static PageManager getPageManager() {
+        if (pageManager == null) {
+            pageManager = new PageManager();
+        }
+        return pageManager;
+    }
+
+    /**
+     * Ленивая инициализация {@link HomePage}
+     *
+     * @return HomePage
+     */
+    public HomePage getHomePage() {
+        if (homePage == null) {
+            homePage = new HomePage();
+        }
+        return homePage;
+    }
+
+    /**
+     * Ленивая инициализация {@link SearchingResultsPage}
+     *
+     * @return ResultsPage
+     */
+    public SearchingResultsPage getSearchingResultsPage() {
+        if (searchingResultsPage == null) {
+            searchingResultsPage = new SearchingResultsPage();
+        }
+        return searchingResultsPage;
+    }
+
+    public ShoppingBasketPage getShoppingBasketPage() {
+        if (shoppingBasketPage == null) {
+            shoppingBasketPage = new ShoppingBasketPage();
+        }
+        return shoppingBasketPage;
+    }
+
+
+}
